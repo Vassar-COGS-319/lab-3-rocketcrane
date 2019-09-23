3087efaa6ae98546c3a8950e368a45e9b8cf07cf
rocketcrane <47123365+rocketcrane@users.noreply.github.com>
2019-09-23 16:41:05 -0400

    Update 3-model-selection.R

--- 3-model-selection.R
+++ 3-model-selection.R
@@ -14,6 +14,109 @@
 
 # You don't need to get a perfect match. Just get in the ballpark. 
 
+# RANDOM WALK MODEL!!!!!!! ####
+random.walk.model <-
+  function(samples, drift = 0.0125, sdrw = 0.3, criterion = 4.921) {
+    theNumber <- 0
+    howMany <- 0
+    accuracy.array <- numeric()
+    rt.array <- numeric()
+    
+    # repeat for number of samples
+    for (i in 1:samples) {
+      theNumber <- 0 # start walk at 0
+      howMany <- 0 # start counter at 0
+      
+      # one sample
+      while ((theNumber <= criterion) && (theNumber >= (0 - criterion))) {
+        theNumber <- (theNumber + rnorm(1, mean = drift, sd = sdrw)) # add to "theNumber," to keep track of walk
+        howMany <- (howMany + 1) # increment counter by 1
+      }
+      
+      # output correct or not
+      if(theNumber >= criterion) {
+        accuracy.array[i] <- TRUE
+      }
+      else {
+        accuracy.array[i] <- FALSE
+      }
+      
+      # output how many
+      rt.array[i] <- howMany
+    }
+    
+    output <- data.frame(correct = accuracy.array,
+                         rt = rt.array)
+    
+    return(output)
+  }
+
+# test the model ####
+library(dplyr)
+randomwalk.test <- random.walk.model(1000)
+
+sum(randomwalk.test$correct) / length(randomwalk.test$correct) # should be close to 0.5
+mean(randomwalk.test$rt) # should be about 112
+
+walkcorrect.data <- randomwalk.test %>% filter(correct == TRUE)
+walkincorrect.data <- randomwalk.test %>% filter(correct == FALSE)
+mean(walkcorrect.data$rt) # correct mean
+mean(walkincorrect.data$rt) # incorrect mean
+
+#ACCUMULATOR MODEL!!!!!!! ####
+accumulator.model <- function(samples, rate.1=82.5, rate.2=89, criterion=3){
+  theNumber.1 <- 0 # accumulator 1
+  theNumber.2 <- 0 # accumulator 2
+  howMany <- 0 # number of samples
+  
+  # initialize arrays
+  accuracy.array <- numeric()
+  rt.array <- numeric()
+  
+  # repeat for number of samples
+  for (i in 1:samples) {
+    theNumber.1 <- 0 # start walk.1 at 0
+    theNumber.2 <- 0 # start walk.1 at 0
+    howMany <- 0 # start counter at 0
+    
+    # one sample
+    while ((theNumber.1 <= criterion) && (theNumber.2 <= criterion)) {
+      theNumber.1 <- (theNumber.1 + rexp(1, rate.1)) # add to "theNumber.1," to keep track of walk
+      theNumber.2 <- (theNumber.2 + rexp(1, rate.2)) # add to "theNumber.1," to keep track of walk
+      howMany <- (howMany + 1) # increment counter by 1
+    }
+    
+    # output correct or not
+    if(theNumber.1 >= criterion || theNumber.1 >= theNumber.2) {
+      accuracy.array[i] <- TRUE
+    }
+    else {
+      accuracy.array[i] <- FALSE
+    }
+    
+    # output how many
+    rt.array[i] <- howMany
+  }
+  
+  output <- data.frame(
+    correct = accuracy.array,
+    rt = rt.array
+  )
+  
+  return(output)
+}
+
+# test the model ####
+acc.test <- accumulator.model(1000)
+acccorrect.data <- acc.test %>% filter(correct == TRUE)
+accincorrect.data <- acc.test %>% filter(correct == FALSE)
+# MEAN: should be close to 0.8
+sum(acc.test$correct) / length(acc.test$correct)
+# correct mean
+mean(acccorrect.data$rt)
+# incorrect mean
+mean(accincorrect.data$rt)
+
 
 # Can both models do a reasonable job of accounting for the mean RT and accuracy? Report the
 # results of your efforts:
